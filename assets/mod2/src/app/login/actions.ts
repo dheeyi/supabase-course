8903715cd72a1bd6a3bb2f1b6a00142b7612afde
William Barra <dheeyi@gmail.com>
2025-07-08 15:02:33 -0400

    Create actions.ts

---
+++ assets/mod2/src/app/login/actions.ts
@@ -0,0 +1,44 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/account')
+}
+
+export async function signUp(formData: FormData) {
+  const supabase = await createClient()
+
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error, data } = await supabase.auth.signUp(data)
+  console.log('error', error);
+  console.log('data', data);
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/account')
+}
