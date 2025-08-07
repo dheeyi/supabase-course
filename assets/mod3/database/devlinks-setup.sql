2f65b43f671a67d1032070821243b96c49f1e8c2
William Barra <dheeyi@gmail.com>
2025-08-07 02:11:23 -0400

    Create devlinks-setup.sql

---
+++ assets/mod3/database/devlinks-setup.sql
@@ -0,0 +1,8 @@
+/**
+  * -------------------------------------------------------
+  *  devlinks schema setup
+  * -------------------------------------------------------
+ */
+
+CREATE SCHEMA IF NOT EXISTS devlinks;
+GRANT USAGE ON SCHEMA devlinks to authenticated;
